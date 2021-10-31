e60994a380a5194ade70106109dc7828095b2dc7
Syeda Maria Manzoor <syeda.maria.manzoor@gmail.com>
2021-10-31 18:02:06 -0400

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,11 @@
+/*
+db.js // dblink 
+Syeda Maria Manzoor
+id: 301184173
+Book Library (Midterm Project)
+*/
+
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://maria:ZyzbdzDglTCRW6Xx@syeda-301184173.tbx3m.mongodb.net/Midterm-301184173?retryWrites=true&w=majority"
+};
